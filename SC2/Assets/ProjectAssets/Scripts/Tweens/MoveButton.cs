a609928dad3773c8d7a8dcc1b6bb0482c74a6273
Diviatrix <Diviatrix@gmail.com>
2016-05-09 14:07:40 +0300

    Initial Commit

---
+++ SC2/Assets/ProjectAssets/Scripts/Tweens/MoveButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveButton: MonoBehaviour {
+
+	public void MoveBy(float f)
+    {
+        gameObject.MoveBy(new Vector3(f, 0, 0), 1, 0);
+    }
+}
